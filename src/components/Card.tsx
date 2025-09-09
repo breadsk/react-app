9142fe2e70ce1c49995e74344b2718710f3de718
Nicolas <nicolas.programador@gmail.com>
2025-09-09 18:53:24 -0300

    Cambios en interfaces en card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,6 +5,7 @@ interface Props{
 
 function Card(props: Props){
 
+    //Desestructurando
     const { body } = props;
 
     return (
@@ -18,14 +19,19 @@ function Card(props: Props){
     )
 }
 
-export function CardBody(){
+interface CardBodyProps{
+    title: string,
+    text: string,
+}
+
+export function CardBody(props: CardBodyProps){
+
+    const { title, text } = props;
+
     return (
         <>
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-            <a href="#" className="btn btn-primary">
-                Go somewhere
-            </a>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{text}</p>
         </>
     )
 }
